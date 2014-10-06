05f4ba8b032fb25e7051c53fbd78f59543b4962e
Josh Bryant <jbryant@shareone.com>
2014-10-06 15:18:35 -0500

    Started the responsive slider on education

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('/');
-	this.resource('home');
-	this.resource('about');
+	this.resource('staff');
 	this.resource('education');
 	this.resource('membership');
 	this.resource('sponsors');// put your routes here
