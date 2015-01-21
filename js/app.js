45add54983f203ae202bf48d1fd19379be09d87f
Josh Bryant <Kaytal@gmail.com>
2015-01-21 13:38:59 -0600

    Updated images and added pieces for Events

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ App = Ember.Application.create();
 App.Router.map(function() {
 	this.resource('staff');
 	this.resource('education');
+    this.resource('events');
 	this.resource('membership');
 	this.resource('sponsors');// put your routes here
 });
